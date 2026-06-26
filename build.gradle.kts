de74726e22d898080424aa4cbabad71a12f5011a
seeyueer <seeyuer@gmail.com>
2026-06-26 19:20:29 +0800

    chore(build): 升级 Gradle 9.3.1, AGP 9.1.1 和 Kotlin 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:${libs.versions.ksp.get()}")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
